6876fc9749148894f13d6e563044fbfcfe58af9f
MYMBASH <91875165+MYMBASH@users.noreply.github.com>
2026-06-01 04:21:06 +0300

    Add files via upload

---
+++ assets/card-BYrRL9lj.js
@@ -0,0 +1,6 @@
+import{c as o,j as t,m as s}from"./index-BDm2FIoe.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=o("House",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]);function n({className:a,...e}){return t.jsxDEV("div",{"data-loc":"client/src/components/ui/card.tsx:7","data-slot":"card",className:s("bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",a),...e},void 0,!1,{fileName:"/home/ubuntu/rcm-proposal-site/client/src/components/ui/card.tsx",lineNumber:7,columnNumber:5},this)}function d({className:a,...e}){return t.jsxDEV("div",{"data-loc":"client/src/components/ui/card.tsx:66","data-slot":"card-content",className:s("px-6",a),...e},void 0,!1,{fileName:"/home/ubuntu/rcm-proposal-site/client/src/components/ui/card.tsx",lineNumber:66,columnNumber:5},this)}export{n as C,r as H,d as a};
---
+++ assets/database-Cihjo5wh.js
@@ -0,0 +1,16 @@
+import{c as e}from"./index-BDm2FIoe.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("ChartColumn",[["path",{d:"M3 3v16a2 2 0 0 0 2 2h16",key:"c24i48"}],["path",{d:"M18 17V9",key:"2bz60n"}],["path",{d:"M13 17V5",key:"1frdt8"}],["path",{d:"M8 17v-3",key:"17ska0"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=e("Code",[["polyline",{points:"16 18 22 12 16 6",key:"z7tu5w"}],["polyline",{points:"8 6 2 12 8 18",key:"1eg1df"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=e("Database",[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5V19A9 3 0 0 0 21 19V5",key:"1wlel7"}],["path",{d:"M3 12A9 3 0 0 0 21 12",key:"mv7ke4"}]]);export{o as C,s as D,t as a};
